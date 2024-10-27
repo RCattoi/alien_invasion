a3f20bf8aca3e2e04169a0c0f37d34a50001ab1c
rodrigo cattoi <rodrigocattoi@gmail.com>
2024-10-27 20:40:57 -0300

    Refactor bullet width in settings.py

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 2.5
-        self.bullet_width = 4000
+        self.bullet_width = 4
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
