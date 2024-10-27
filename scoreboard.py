a58d35dd3c9608eb8bf284c1d7cffb86afe0270e
rodrigo cattoi <rodrigocattoi@gmail.com>
2024-10-27 20:40:31 -0300

    refactor: Add level tracking and display in the scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -19,6 +19,16 @@ class Scoreboard:
         self.prep_high_score()
         # Prepare the initial score image.
         self.prep_score()
+        self.prep_level()
+
+    def prep_level(self):
+        lvl_str = str(self.stats.level)
+        self.level_image = self.font.render(
+            lvl_str, True, self.text_color, self.settings.bg_color
+        )
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_score(self):
         score_str = str(self.stats.score)
@@ -35,6 +45,7 @@ class Scoreboard:
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
